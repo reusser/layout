a92743f42aff973090d6e3f2e7796536ea5873cc
oyh <919917215@qq.com>
2017-03-24 21:08:08 +0800

    puzzle layout

--- docs/puzzle/task43.js
+++ docs/puzzle/task43.js
@@ -1,56 +1,54 @@
-var Album = (function() {
-  function fixWrongStyle(node, width, height) {
-    var childCount = node.children.length;
-    if (childCount == 3) {
-      fix3(node.children);
-    }
-    else if (childCount == 5) {
-      fix5(node.children);
-    }
-
-
-    function fix3(images) {
-      var val1 = height / 2, val2 = width - val1;
-      images[0].style.width = val2 + "px";
-      images[1].style.width = val1 + "px";
-      images[1].style.left = val2 + "px";
-      images[2].style.width = val1 + "px";
-      images[2].style.left = val2 + "px";
-    }
-    function fix5(images) {
-      var val1 = width / 3, val2 = height - val1;
-      images[1].style.height = val1 + "px";
-      images[4].style.height = val2 + "px";
-      images[4].style.top = val1 + "px";
-    }
+class Puzzle {
+  constructor(node, width, height) {
+    this.node   = node;
+    this.width  = width;
+    this.height = height
   }
 
-  return {
-    init: function() {
-      var nodes = document.querySelectorAll('.album');
-      [].forEach.call(nodes, function(node) {
-        width = node.dataset.width || node.clientWidth;
-        height = node.dataset.height || node.clientHeight;
-
-        node.style.width = width + "px";
-        node.style.height = height + "px";
-        fixWrongStyle(node, width, height);
-      });
-    },
-    setUp: function(id, options) {
-      var node = document.getElementById(id);
-      node.className += " album";
-
-      options = options || {};
-      width = options.width || node.clientWidth;
-      height = options.height || node.clientHeight;
-
-      node.style.width = width + "px";
-      node.style.height = height + "px";
-
-      fixWrongStyle(node, width, height);
-    }
+  fixStyle() {
+    let imgCount = this.node.getElementsByTagName('img').length;
+    if (imgCount === 3) this.fixThreeImgStyle();
+    else if (imgCount === 5) this.fixFiveimgStyle();
   }
-})();
 
-Album.init();
+  fixThreeImgStyle() {
+    let val1 = this.height / 2;
+    let val2 = this.width - val1;
+    let [img1, img2, img3] = Array.from(this.node.getElementsByTagName('img'));
+    img1.style.width  = `${val2}px`;
+    img2.style.width  = `${val1}px`;
+    img2.style.left   = `${val2}px`;
+    img3.style.width  = `${val1}px`;
+    img3.style.left   = `${val2}px`;
+  }
+
+  fixFiveimgStyle() {
+    let val1 = this.width / 3;
+    let val2 = this.height - val1;
+
+    let img2 = this.node.getElementsByTagName('img')[1];
+    let img5 = this.node.getElementsByTagName('img')[4];
+    img2.style.height = `${val1}px`;
+    img5.style.height = `${val2}px`;
+    img5.style.top    = `${val1}px`;
+  }
+
+  set() {
+    this.node.className += ' puzzle';
+    this.node.style.width  = `${this.width}px`;
+    this.node.style.height = `${this.height}px`;
+    this.fixStyle();
+  }
+
+}
+
+const init = () => {
+  let nodes = document.getElementsByClassName('puzzle');
+  Array.from(nodes).forEach( node => {
+    let width  = node.dataset.width  || node.clientWidth;
+    let height = node.dataset.height || node.clientHeight;
+    new Puzzle(node, width, height).fixStyle();
+  } )
+};
+
+init();
